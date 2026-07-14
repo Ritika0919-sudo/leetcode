775de70bbc0dad863326cf7b76df8dd8955342eb
Ritika0919-sudo <pandeyritika686@gmail.com>
2026-07-15 00:13:50 +0530

    Time: 0 ms (100%), Space: 46.8 MB (43.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,23 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Edge case: An empty array has 0 unique elements
-        if (nums.length == 0) {
+        if(nums.length == 0)
+        {
             return 0;
         }
-        
-        // 'j' tracks the index of the last placed unique element
-        int j = 0;
-        
-        // Iterate through the array starting from the second element
-        for (int i = 1; i < nums.length; i++) {
-            // If we find a value different from the last unique element
-            if (nums[i] != nums[j]) {
-                j++;           // Move to the next slot
-                nums[j] = nums[i]; // Update it with the new unique value
+        else
+        {
+            int j= 0;
+            for(int i = 0; i <nums.length; i++)
+            {
+                if( nums[i] != nums[j])
+                {
+                     j++;
+                     nums[j]= nums[i];
+                }
+            }
+            return j+1;
+               
             }
         }
-        
-        // The number of unique elements is the index 'j' + 1
-        return j + 1;
-    }
 }
