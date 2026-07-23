4071f73cddf1005f3b8cdd4a5e26f86d2da8a265
Ritika0919-sudo <pandeyritika686@gmail.com>
2026-07-23 11:51:55 +0530

    Time: 2 ms (70.41%), Space: 44.7 MB (55.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,6 +2,7 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         backtrack(result, "", 0, 0, n);
+
         return result;
     }
 
