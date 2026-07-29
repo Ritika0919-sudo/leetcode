3ef29233bf861527a2c78367660154a79d5a2c19
Ritika0919-sudo <pandeyritika686@gmail.com>
2026-07-30 02:31:32 +0530

    Time: 15 ms (61.42%), Space: 48.5 MB (60.31%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+
+    public int[] nextGreaterElements(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = -1;
+        }
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+
+            int idx = i % n;
+
+            while (!stack.isEmpty() && stack.peek() <= nums[idx]) {
+                stack.pop();
+            }
+
+            if (i < n) {
+                if (!stack.isEmpty()) {
+                    ans[idx] = stack.peek();
+                }
+            }
+
+            stack.push(nums[idx]);
+        }
+
+        return ans;
+    }
+}
